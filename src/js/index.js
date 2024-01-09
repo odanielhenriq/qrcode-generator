208df13abc435b6645bd159f95bdab3380021011
Daniel <daniel.henrique00@hotmail.com>
2024-01-08 22:07:00 -0300

    feat: button to download QRCode

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 let imgBox = document.getElementById("imgBox");
 let qrImage = document.getElementById("qrImage");
 let qrText = document.getElementById("qrText");
+let downloadButton = document.getElementById("downloadButton");
 
 function GenerateQR() {
     if (qrText.value.length > 0) {
@@ -10,4 +11,21 @@ function GenerateQR() {
     }
 }
 
+function downloadQR() {
+
+    if (qrText.value.length > 0) {
+
+        downloadButton.setAttribute('download', 'qrcode.png')
+
+        downloadButton.href = `https://api.qrserver.com/v1/create-qr-code/?size=1000x1000&data=${qrText.value}&download=1`
+    } else {
+        qrImage.src = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=example';
+        qrText.classList.add('error')
+        setTimeout(() => {
+            qrText.classList.remove('error')
+        }, 1000);
+    }
+}
+
 qrText.addEventListener('input', GenerateQR);
+downloadButton.addEventListener('click', downloadQR); 
